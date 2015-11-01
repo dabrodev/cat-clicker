3f0da72e9d55c0ff053f5dfee8d91bfee16fec5c
Damian Dabrowski <dabdamian@gmail.com>
2015-11-01 20:13:12 +0100

    refactor: updated js file

--- js/script.js
+++ js/script.js
@@ -1,14 +1,14 @@
 var counter = 0;
 
 var elem = document.getElementById('cat-pic');
-var p = document.createElement("span");
-p.innerHTML = 0;
-document.getElementById("log").appendChild(p);
+var counterArea = document.createElement("span");
+counterArea.innerHTML = 0;
+document.getElementById("log").appendChild(counterArea);
 
 elem.addEventListener('click', function(){
   counter += 1;
 
-  p.innerHTML = counter;
-  document.getElementById("log").appendChild(p);
+  counterArea.innerHTML = counter;
+  document.getElementById("log").appendChild(counterArea);
 
 }, false);
